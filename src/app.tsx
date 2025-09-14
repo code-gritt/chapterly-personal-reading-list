192b92aabb2c7c24574d90819fadbd48f400e545
Gokul <gokulchandan24@gmail.com>
2025-09-15 02:10:54 +0530

    a

--- src/app.tsx
+++ src/app.tsx
@@ -1,25 +1,32 @@
-import { Banner } from './components/banner'
-import { CallToAction } from './components/call-to-action'
-import { Faqs } from './components/faqs'
-import { Features } from './components/features'
-import { Footer } from './components/footer'
-import { Hero } from './components/hero'
-import { LogoTicker } from './components/logo-ticker'
-import { Navbar } from './components/navbar'
-import { ProductShowcase } from './components/product-showcase'
+import { Banner } from "./components/banner";
+import { CallToAction } from "./components/call-to-action";
+import { Faqs } from "./components/faqs";
+import { Features } from "./components/features";
+import { Footer } from "./components/footer";
+import { Hero } from "./components/hero";
+import { LogoTicker } from "./components/logo-ticker";
+import { Navbar } from "./components/navbar";
+import { ProductShowcase } from "./components/product-showcase";
 
 export function App() {
   return (
     <div className="min-h-screen antialiased">
-      <Banner />
-      <Navbar />
-      <Hero />
-      <LogoTicker />
-      <Features />
-      <ProductShowcase />
-      <Faqs />
-      <CallToAction />
-      <Footer />
+      {/* Fixed header */}
+      <div className="fixed left-0 right-0 top-0 z-50">
+        <Banner />
+        <Navbar />
+      </div>
+
+      {/* Main content with top padding (height of Banner + Navbar) */}
+      <div className="pt-[120px]">
+        <Hero />
+        <LogoTicker />
+        <Features />
+        <ProductShowcase />
+        <Faqs />
+        <CallToAction />
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
